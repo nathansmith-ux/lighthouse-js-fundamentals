5ad819a7c35930c791a789ebdf2875c6c55f478a
Nathan Smith <nathanrmsmith@gmail.com>
2023-06-27 17:06:07 -0700

    Is the number odd?

---
+++ odd.js
@@ -0,0 +1,11 @@
+function isOdd(num){
+  if (num % 2 !== 0){
+    return true
+  } else {
+    return false
+  } 
+}
+
+// Checking if it is odd
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
